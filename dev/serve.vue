f03f0558c9838ae52e9307db809b9e16ce563911
Junji Zhi <junjizhi.to@gmail.com>
2021-06-06 11:23:54 -0400

    initial commit

---
+++ dev/serve.vue
@@ -0,0 +1,17 @@
+<script>
+import Vue from 'vue';
+import BootstrapVueTimeline from '@/bootstrap-vue-timeline.vue';
+
+export default Vue.extend({
+  name: 'ServeDev',
+  components: {
+    BootstrapVueTimeline
+  }
+});
+</script>
+
+<template>
+  <div id="app">
+    <bootstrap-vue-timeline />
+  </div>
+</template>
